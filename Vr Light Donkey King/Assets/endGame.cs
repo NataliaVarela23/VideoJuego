a28232fe191136155de0f5c8fe3a05e036586e77
Ignacio759 <ignacio.veliz@virginiogomez.cl>
2019-12-18 23:21:38 -0300

    Setup General
    Plataformas permiten llegar hasta la cima de la montaña (sin decoraciones ni trampas).
    
    Setup general de la escena de estadísticas.

---
+++ Vr Light Donkey King/Assets/endGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("Estadisticas");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
