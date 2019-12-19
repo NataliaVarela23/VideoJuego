86a37b8a461960c798e448a9514310b1b43b6680
Ignacio759 <ignacio.veliz@virginiogomez.cl>
2019-12-19 17:57:55 -0300

    Añado cuadro de diálogo
    almaceno varios diálogos en un mismo objeto y se muestran satisfactoriamente en la pantalla en forma de cuadro de diálogo, al finalizar el diálogo se salta a la pantalla de estadísticas.

--- Vr Light Donkey King/Assets/Cambio escenas/endGame.cs
+++ Vr Light Donkey King/Assets/Cambio escenas/endGame.cs
@@ -5,21 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class endGame : MonoBehaviour
 {
+    public Dialogos Dialogo;//asignar script a trigger
+    public Dialogos[] hijo;
+    public int wisem;
     // Start is called before the first frame update
-    void Start()
+    public void TriggerDialogue()
     {
-        
+        FindObjectOfType<DialogManager>().StartDialogue(hijo[wisem]);//permite almacenar varias posibilidades de diálogo dentro de un mismo objeto, usando la misma pauta
+        //FindObjectOfType<DialogManager>().StartDialogue(Dialogo);//permite acceder al dialogo único almacenado en el personaje
     }
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene("Estadisticas");
+            
+            wisem = Random.Range(0, hijo.Length-1);
+            TriggerDialogue();
         }
     }
     // Update is called once per frame
-    void Update()
+    public void EndGame()
     {
-        
+        SceneManager.LoadScene("Estadisticas");
     }
 }
---
+++ Vr Light Donkey King/Assets/UI player/DialogManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    private Queue<string> oraciones;//asignar script a objeto manager
+
+    public Text Nombre;
+    public Text cuerpo;
+    public Animator cuadro;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oraciones = new Queue<string>();
+    }
+
+
+    public void StartDialogue(Dialogos dialogo)
+    {
+        cuadro.SetBool("IsOpen",true);
+        Nombre.text = dialogo.Name ;
+
+
+        oraciones.Clear();
+        foreach (string sentence in dialogo.oracion)
+        {
+            oraciones.Enqueue(sentence);
+        }
+
+        NextSentence();
+
+    }
+
+    public void NextSentence()
+    {
+
+
+        if (oraciones.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = oraciones.Dequeue();
+        //cuerpo.text = sentence;
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence (string sentence)
+    {
+        cuerpo.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            cuerpo.text += letter;
+            yield return null;//espera un frame y permite funcionar a corrutina
+        }
+    }
+    public void EndDialogue()
+    {
+        cuadro.SetBool("IsOpen", false);
+        FindObjectOfType<endGame>().EndGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            NextSentence();
+        }
+    }
+}
